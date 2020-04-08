b148552fccb1ac100767d0872d6b67cff1739db7
alasbri1 <60518854+alasbri1@users.noreply.github.com>
2020-04-08 17:05:40 -0400

    Minor changed
    Corrected the spelling of district
    Added comments to specify expected output
    Changed population of dummy districts
    Added a check for which districts were added, and which districts were ignored

--- algorithm_practice.cpp
+++ algorithm_practice.cpp
@@ -2,18 +2,19 @@
 #include <list>
 using namespace std;
 
-struct Disctict
+struct District
 {
 	double m_population;
 	bool m_party;
 	double m_percentage;
-	list<Disctict *> m_neighbors;
+	list<District *> m_neighbors;
 };
 
-int main(){
+int main()
+{
 
 	//Creating dummy districts
-	Disctict dis1, dis2, dis3, dis4, dis5, dis6, dis7, dis8;
+	District dis1, dis2, dis3, dis4, dis5, dis6, dis7, dis8;
 
 	//Each district is given a default party affiliation
 	dis1.m_party = true;
@@ -25,17 +26,17 @@ int main(){
 	dis7.m_party = true;
 	dis8.m_party = false;
 
-	//Each district is given a population of 10
+	//Each district is given a population divisible by 10
 	dis1.m_population = 10;
-	dis2.m_population = 10;
-	dis3.m_population = 10;
-	dis4.m_population = 10;
-	dis5.m_population = 10;
-	dis6.m_population = 10;
-	dis7.m_population = 10;
-	dis8.m_population = 10;
+	dis2.m_population = 20;
+	dis3.m_population = 30;
+	dis4.m_population = 40;
+	dis5.m_population = 50;
+	dis6.m_population = 60;
+	dis7.m_population = 70;
+	dis8.m_population = 80;
 
-	//Each district is given a dominant percentage of 70%
+	//Each district is given a dominant percentage of 80%
 	dis1.m_percentage = .8;
 	dis2.m_percentage = .8;
 	dis3.m_percentage = .8;
@@ -75,19 +76,22 @@ int main(){
 
 	/*Iterator for the list has to be seperate because otherwise
 	I end up adding the same thing over and over again */
-	list<Disctict *>::iterator it = dis1.m_neighbors.begin();
-	list<Disctict *>::iterator it2 = dis2.m_neighbors.begin();
+	list<District *>::iterator it = dis1.m_neighbors.begin();
+	list<District *>::iterator it2 = dis2.m_neighbors.begin();
 	
-	while(addDis < 7 && time < 20){
+	while(addDis < 7 && time < 20)
+	{
 		time++;
 		//While the district still has a choice to make
 		bool choice = true;
 
-		while(choice){
+		while(choice)
+		{
 
 			/*Each district will go through its neighbors to 
 			find one to add */
-			for(it; it!=dis1.m_neighbors.end();++it){
+			for(it; it!=dis1.m_neighbors.end();++it)
+			{
 
 				/*If the neighboring district, really the precinct,
 				is of the same party, then add it and continue */
@@ -97,7 +101,7 @@ int main(){
 					/*So, to add the district, add the population, fix
 					the percentage, and fix the neighbors as needed */
 
-					int newTotal = ((dis1.m_population * dis1.m_percentage) + ((*it)->m_percentage * (*it)->m_population));
+					int newTotal = ((dis1.m_population * dis1.m_percentage) + ((*it)->m_population * (*it)->m_percentage));
 					dis1.m_population = dis1.m_population + (*it)->m_population;
 					dis1.m_percentage = newTotal / dis1.m_population;
 					dis1.m_neighbors.merge((*it)->m_neighbors);
@@ -116,9 +120,11 @@ int main(){
 		}
 
 		choice = true;
-		while(choice){
+		while(choice)
+		{
 
-			for(it2; it2!=dis2.m_neighbors.end();++it2){
+			for(it2; it2!=dis2.m_neighbors.end();++it2)
+			{
 
 				/*If the neighboring district, really the precinct,
 				is of the same party, then add it and continue */
@@ -127,7 +133,7 @@ int main(){
 					/*So, to add the district, add the population, fix
 					the percentage, and fix the neighbors as needed */
 					
-					int newTotal = ((dis2.m_population * dis2.m_percentage) + ((*it2)->m_percentage * (*it2)->m_population));
+					int newTotal = ((dis2.m_population * dis2.m_percentage) + ((*it2)->m_population * (*it2)->m_percentage));
 					dis2.m_population = dis2.m_population + (*it2)->m_population;
 					dis2.m_percentage = newTotal / dis2.m_population;
 					dis2.m_neighbors.merge((*it2)->m_neighbors);
@@ -148,15 +154,28 @@ int main(){
 
 	}
 
+	//Checking district information
 	cout << "District 1 info:" << endl;
 	cout << "Party: " << dis1.m_party << endl;
-	cout << "Population: " << dis1.m_population << endl;
-	cout << "Percentage: " << dis1.m_percentage << endl;
+	cout << "Population: " << dis1.m_population << endl; //Should be 130
+	cout << "Percentage: " << dis1.m_percentage << endl; //Should be .8
 
 	cout << "District 2 info: " << endl;
 	cout << "Party: " << dis2.m_party << endl;
-	cout << "Population: " << dis2.m_population << endl;
-	cout << "Percentage: " << dis2.m_percentage << endl;
+	cout << "Population: " << dis2.m_population << endl; //Should be 120
+	cout << "Percentage: " << dis2.m_percentage << endl; //Should be .8
+
+	cout << "Check incorperated districts:" << endl;
+
+	//Any districts that were added to either dis1 or dis2 should be empty
+	cout << dis1.m_neighbors.empty() << endl; //Should be false
+	cout << dis2.m_neighbors.empty() << endl; //Should be false
+	cout << dis3.m_neighbors.empty() << endl; //Should be false
+	cout << dis4.m_neighbors.empty() << endl; //Should be true
+	cout << dis5.m_neighbors.empty() << endl; //Should be true
+	cout << dis6.m_neighbors.empty() << endl; //Should be true
+	cout << dis7.m_neighbors.empty() << endl; //Should be true
+	cout << dis8.m_neighbors.empty() << endl; //Should be true
 
 	return 1;
 }
