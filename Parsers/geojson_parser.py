211fd62e594a2cb56f21184f324b06def03df5ee
Lukas Mueller <lukas1@umbc.edu>
2020-04-20 00:13:48 -0400

    md_precinct2010.geojson now has a field for each precinct that contains its neighboring precincts. geojson_parser.py now parses the MD geojson data to write a txt file containing each precinct and its neighbors (md_precinct_neighbors.txt). md_county_codes.csv now has column headers.

--- Parsers/geojson_parser.py
+++ Parsers/geojson_parser.py
@@ -2,6 +2,7 @@
 #This program parses the geojson files for the necessary data.
 
 import json
+import csv
 
 #Gets the county codes for each county in Maryland and 
 #stores them as key, value pairs in a dictionary
@@ -12,6 +13,7 @@ def parseMDCountyCodes(filename):
     with open(filename) as precinct_file:
         data = json.load(precinct_file)
 
+    #populate the dictionary
     for feature in data["features"]:
         line = feature["properties"]["NAME"] + ", " + feature["properties"]["COUNTY"]
         line = line.split("Precinct")
@@ -21,14 +23,43 @@ def parseMDCountyCodes(filename):
 
     return countyCodes
 
+#Gets the neighboring precincts of each precinct in Maryland
+#and stores them as key, value pairs in a dictionary
+def parseMDPrecNbrs(filename):
+
+    precNbrs = {}
+
+    with open(filename) as precinct_file:
+        data = json.load(precinct_file)
+
+    #populate the dictionary
+    for feature in data["features"]:
+        precNbrs[feature["properties"]["VTD"]] = feature["properties"]["NEIGHBORS"]
+        
+    return precNbrs
 
 def main():
 
     #write all the counties + their codes to a csv
-    county_file = open("md_county_codes.csv", "w+")
-    countyCodes = parseMDCountyCodes("precinct2010.geojson")
+    county_file = open("md_county_codes.csv", "w+", newline='')
+    countyCodes = parseMDCountyCodes("md_precinct2010.geojson")
+
+    writer = csv.writer(county_file)
+    writer.writerow(["County", "Code"])
+    
     for county in countyCodes:
-        county_file.write(county + ", " + countyCodes[county] + "\n")
+        writer.writerow([county, countyCodes[county]])
     county_file.close()
+
+    #write all precincts + their neighbors to a file
+    nbr_file = open("md_precinct_neighbors.txt", "w+")
+    precNbrs = parseMDPrecNbrs("md_precinct2010.geojson")
+    for precinct in precNbrs:
+        #6 precincts do not have neighbor lists
+        if precNbrs[precinct]:
+            nbr_file.write(precinct + ": " + precNbrs[precinct] + "\n")
+        else:
+            nbr_file.write(precinct + "\n")
+    nbr_file.close()
 
 main()
