b90f825b15f35a864365996ef1f315102749beb1
Phillip Chow <SupremeChow@gmail.com>
2020-05-07 00:37:26 -0400

    Added validity check for South Carolina

--- wireframe/Hash_Check.js
+++ wireframe/Hash_Check.js
@@ -55,8 +55,28 @@ window.onload = function()
 				
 				case 'South Carolina':
 				{
-					//Do nothing, wait until processed data is ready and already hashed
-					alert("South Carolina Keys not ready");
+					//Take the file currently in directory, and encrypt 
+					
+					theHash = sjcl.encrypt("abcde", fileContent,{iv:"m0aLuLijId99uhZpe9lFnQ==",v:1,iter:10000,ks:128,ts:64,mode:"ccm",adata:"",cipher:"aes",salt:"uUcE3UaZMfQ="});
+					
+					//Remove useless info, keep only important things to compare
+					var parsedMessage = JSON.parse(theHash);
+					delete parsedMessage.mode;
+					delete parsedMessage.iter;
+					delete parsedMessage.ks;
+					delete parsedMessage.ts;
+					delete parsedMessage.v;
+					delete parsedMessage.cipher;
+					delete parsedMessage.salt;
+					delete parsedMessage.adata;
+					
+					// !!NOTE!! Again, hard coding the correct values we should expect
+					if(parsedMessage.ct.substring(0, 50) != "iOOBJawy8mDi6Jc8EIJo19VEhmO+grZ6E75MFBc3GyrXdHA5su")
+					{
+						alert("South Carolina Data is Invalid!");
+					}
+					else
+						alert("South Carolina Valid");
 					break;
 				}
 				
